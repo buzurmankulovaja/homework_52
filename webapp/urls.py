02773b7ccd60f84c6433aae3ee2d22d33d00b4b6
jypara <buzurmankulovaja@gmail.com>
2025-10-31 15:29:05 +0600

    добавила страницу для детального просмотра задачи

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from webapp.views import task_list_view, task_create_view
+from webapp.views import task_list_view,task_detail_view,task_create_view
+
 
 urlpatterns = [
-    path('', task_list_view),
-    path('task/create/', task_create_view),
+    path('', task_list_view, name='task_list'),
+    path('task/<int:pk>/', task_detail_view, name='task_detail'),
+    path('task/create/', task_create_view, name='task_create'),
 ]

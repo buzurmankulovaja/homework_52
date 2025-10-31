aad821451366341740b2ae5d1202d19fbf0b59ba
jypara <buzurmankulovaja@gmail.com>
2025-10-31 15:37:48 +0600

    обновлены админка, сеть и представления

--- webapp/admin.py
+++ webapp/admin.py
@@ -6,8 +6,8 @@ from webapp.models import Task
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'description', 'due_date', 'created_at', 'updated_at')
     list_filter = ['due_date']
-    search_fields = ('description', 'status')
-    fields = ('description', 'status', 'due_date', 'created_at', 'updated_at')
+    search_fields = ('description','detailed_description', 'status')
+    fields = ('description','detailed_description', 'status', 'due_date', 'created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
 
 
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from webapp.models import Task, status_choices
 
@@ -10,12 +9,18 @@ def task_list_view(request):
     }
     return render(request, 'task_list.html', context)
 
+def task_detail_view(request, pk,):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'task_detail.html', {'task': task})
+
+
 def task_create_view(request):
     if request.method == 'GET':
         return render(request, 'task_create.html', {'status_choices': status_choices})
     elif request.method == 'POST':
         description = request.POST.get('description')
+        detailed_description = request.POST.get('detailed_description')
         status = request.POST.get('status')
-        due_date = request.POST.get('due_date')
-        Task.objects.create(description=description, status=status, due_date=due_date)
-        return HttpResponseRedirect('/')
+        due_date = request.POST.get('due_date') or None
+        task = Task.objects.create(description=description, detailed_description=detailed_description, status=status, due_date=due_date)
+        return redirect('task_detail', pk=task.id)
